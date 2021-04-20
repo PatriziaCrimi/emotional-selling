acad40fa25d29d9b9c1fbe8e67373e50c587a56c
Patrizia <crimi.patrizia@gmail.com>
2021-04-20 10:55:17 +0200

    Rankings bugs

--- resources/views/logged/rankings.blade.php
+++ resources/views/logged/rankings.blade.php
@@ -1,16 +1,17 @@
 @extends('layouts.app')
 
-@section('content')
+@section('scripts')
+  <script src="https://cdn.jsdelivr.net/npm/chart.js@3.1.1/dist/chart.min.js"></script>
+@endsection
+
+@section('chart')
 <div class="container">
   <div class="row justify-content-center">
-
     <div class="col-12">
       <h1 class="text-center">Classifica Provvisoria</h1>
     </div>
-
   </div>
-  ​​​@foreach ($votesRank as $key => $value)
-
+  @foreach ($votesRank as $key => $value)
     <div class="card" style="width: 18rem;">
       <ul class="list-group list-group-flush">
         <li class="list-group-item">
@@ -20,80 +21,69 @@
         </li>
       </ul>
     </div>
-
   @endforeach
-  {{-- <canvas id="myChart" width="400" height="400"></canvas> --}}
+  <canvas id="myChart" width="400" height="400"></canvas>
   <div class="">
-
     <p>classifica visualizzabile da tutti</p>
-
     @if ($round->name == 3)
-     <a class="btn btn-dark" href="{​​​​​​​{​​​​​​​route('logged.final')}​​​​​​​}​​​​​​">Clicca per uscire</a>
+      <a href="{{route('logged.final')}}" class="btn btn-dark">Clicca per uscire</a>
     @else
-     <a class="btn btn-dark" href="{​​​​​​​{​​​​​​​route('logged.index')}​​​​​​​}​​​​​​​">Vai al prossimo round</a>
+      <a href="{{route('logged.index')}}" class="btn btn-dark">Vai al prossimo round</a>
     @endif
-
   </div>
-{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script> --}}
-
 </div>
-{{--
 <script>
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {​​​​​​​
-  type: 'bar',
-  data: {​​​​​​​
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
       labels: [@php
-        foreach ($votesRank as $key => $value) {​​​​​​​
+        foreach ($votesRank as $key => $value) {
           echo "'".
           $value['name'].
           " ".
           $value['lastname'].
           "'".
           ',';
-        }​​​​​​​
+        }
       @endphp],
-      datasets: [{​​​​​​​
+      datasets: [{
           label: 'Voti',
           data: [@php
-            foreach ($votesRank as $key => $value) {​​​​​​​
-              echo
-              $value['valore'].
-
-
+            foreach ($votesRank as $key => $value) {
+              echo $value['valore'].
               ',';
-            }​​​​​​​
+            }
           @endphp],
           backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)'
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
           ],
           borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)'
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
           ],
           borderWidth: 1
-      }​​​​​​​]
-  }​​​​​​​,
-  options: {​​​​​​​
+      }]
+  },
+  options: {
       indexAxis: 'y',
-  }​​​​​​​
-}​​​​​​​);
-function init(){​​​​​​​
-myChart.update();
-}​​​​​​​
-setInterval(function(){​​​​​​​
-init();
-console.log('chart is updatig');
-}​​​​​​​,2000);
-</script> --}}
-
+    }
+  });
+  function init() {
+    myChart.update();
+  }
+  setInterval(function() {
+    init();
+    console.log('chart is updating');
+  }, 2000);
+</script>
 @endsection
