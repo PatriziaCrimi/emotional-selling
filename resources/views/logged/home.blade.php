c1b04ef6f5052951668a1ec2b6ade9c03dc5fcc3
Patrizia <crimi.patrizia@gmail.com>
2021-04-26 15:23:08 +0200

    View Home logged frontend completed

--- resources/views/logged/home.blade.php
+++ resources/views/logged/home.blade.php
@@ -4,7 +4,7 @@
   <section id="home">
     <div class="container">
       <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-12 col-sm-10">
           <div class="card">
             <div class="card-body">
               @if (session('status'))
@@ -12,27 +12,23 @@
                   {{ session('status') }}
                 </div>
               @endif
-              <h1 class="text-center">
-                Benvenuto
-                {{Auth::user()->name}}!
-              </h1>
+              <div class="text-left">
+                <h1>Benvenuto,</h1>
+                <h2>
+                  <span>{{Auth::user()->name}}</span>
+                  <span>{{Auth::user()->lastname}}</span>
+                </h2>
+              </div>
+              <div class="text-center">
+                <p>Clicca il pulsante per vedere il Round in corso</p>
+              </div>
+              <div>
+                <a class="btn btn-block" href="{{route('logged.groups.index')}}">
+                  Vedi Round in corso
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-12">
-          <div class="text-center">
-            <p>Clicca il pulsante per iniziare il Round</p>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="offset-sm-2 col-sm-8">
-          <a class="btn btn-dark btn-block" href="{{route('logged.groups.index')}}">
-            Visualizza il Round in corso
-            {{-- {{$round->name}} --}}
-          </a>
         </div>
       </div>
     </div>  {{-- Closing Container --}}
